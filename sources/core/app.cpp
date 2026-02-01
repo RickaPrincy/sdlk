faba4dd63dd7f50a16e90c1cab94301a4ad0a13b
RickaPrincy <rckprincy@gmail.com>
2026-02-01 15:41:36 +0300

    wip: active dpi only from imgui side

--- sources/core/app.cpp
+++ sources/core/app.cpp
@@ -10,7 +10,6 @@
 #include <sdlk/core/events/event_listener.hpp>
 #include <stdexcept>
 
-#include "imgui.h"
 #include "imgui/imgui_wrapper.hpp"
 
 namespace sdlk
@@ -55,8 +54,7 @@ namespace sdlk
 			SDL_WINDOWPOS_CENTERED,
 			window_width,
 			window_height,
-			SDL_WINDOW_OPENGL | SDL_WINDOW_RESIZABLE | SDL_WINDOW_SHOWN /*|
-			SDL_WINDOW_ALLOW_HIGHDPI */);
+			SDL_WINDOW_OPENGL | SDL_WINDOW_RESIZABLE | SDL_WINDOW_SHOWN | SDL_WINDOW_ALLOW_HIGHDPI);
 
 		this->m_opengl_context = SDL_GL_CreateContext(this->p_window);
 
@@ -142,6 +140,23 @@ namespace sdlk
 				glClear(GL_COLOR_BUFFER_BIT);
 
 				imgui_wrapper::new_frame();
+
+				{
+					static float f = 0.0f;
+					static int counter = 0;
+
+					ImGui::Begin("Hello, world!");                          // Create a window called "Hello, world!" and append into it.
+
+					ImGui::Text("This is some useful text.");               // Display some text (you can use a format strings too)
+
+					ImGui::SliderFloat("float", &f, 0.0f, 1.0f);            // Edit 1 float using a slider from 0.0f to 1.0f
+
+					if (ImGui::Button("Button"))                            // Buttons return true when clicked (most widgets return true when edited/activated)
+						counter++;
+					ImGui::SameLine();
+					ImGui::Text("counter = %d", counter);
+					ImGui::End();
+				}
 
 				this->m_program->use();
 				this->m_camera->load_uniforms(this->m_program);
--- sources/core/imgui/imgui_wrapper.cpp
+++ sources/core/imgui/imgui_wrapper.cpp
@@ -33,13 +33,10 @@ namespace sdlk
 		io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;
 		io.ConfigFlags |= ImGuiConfigFlags_NavEnableGamepad;
 
-		/*
-		 *DPI
-			const auto dpi = get_dpi();
-			auto &style = ImGui::GetStyle();
-			style.ScaleAllSizes(dpi);
-			style.FontScaleDpi = dpi;
-		*/
+		const auto dpi = get_dpi();
+		auto &style = ImGui::GetStyle();
+		style.ScaleAllSizes(dpi);
+		style.FontScaleDpi = dpi;
 
 		ImGui_ImplSDL2_InitForOpenGL(window, gl_context);
 		ImGui_ImplOpenGL3_Init(glsl_version.c_str());
